06abf66e11dd0362a2ba129d3c6c9a7aea131414
Nadine Friess <kontakt@nadinefriess.de>
2021-01-26 18:36:02 +0100

    project setup and app prototype

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,61 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  }));
 
   it(`should have as title 'illugen'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('illugen');
+    const component = fixture.componentInstance;
+    expect(component.title).toEqual('Illugen');
   });
 
-  it('should render title', () => {
+  it(`should render a toolbar with app title, overview and settings icon`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const toolbar = fixture.nativeElement.querySelector('.toolbar')
+    const title = fixture.nativeElement.querySelector('.title')
+    const overview = fixture.nativeElement.querySelector('.overview')
+    const settings = fixture.nativeElement.querySelector('.settings')
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('illugen app is running!');
+    expect(toolbar).toBeDefined();
+    expect(title.innerText).toEqual('Illugen');
+    expect(overview.textContent).toEqual('remove_red_eye');
+    expect(settings.textContent).toEqual('settings');
   });
-});
+
+  it(`should render a hint text`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const hint = fixture.nativeElement.querySelector('.hint')
+    expect(hint).toBeDefined();
+  });
+
+  it('should render generate button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const button = fixture.nativeElement.querySelector('.generate')
+    fixture.detectChanges();
+    expect(button.textContent).toContain('start');
+  });
+
+  it('should call generate function ', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const button = fixture.nativeElement.querySelector('.generate')
+    spyOn(component, 'onGenerateClick');
+    button.click();
+    fixture.detectChanges()
+    expect(component.onGenerateClick).toHaveBeenCalled();
+  }));
+
+  it('should render list', ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const button = fixture.nativeElement.querySelector('.generate');
+    button.click();
+    fixture.detectChanges();
+    const list = fixture.nativeElement.querySelector('.resultList');
+    expect(list).toBeDefined();
+  });
+
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
