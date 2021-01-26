9f04398a97755a2a0f83bc7d4d33242e19c9b657
Nadine Friess <kontakt@nadinefriess.de>
2021-01-26 20:56:27 +0100

    simplification of methods and outsource test data to own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,45 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Illugen';
-  categoryList = [{
-      category:'Körper',
-      term:['Kegel','Würfel','Kugel','Quader','Prisma','Pyramide','Zylinder']
-    },{
-      category:'Formen',
-      term:['Quadrat','Rechteck','Trapez','Dreieck','Raute','Kreis','Parrallelogramm','Ellipse','Mehreck']
-    },{
-      category:'Farben',
-      term:['Rot','Gelb','Blau','Grün','Orange','Lila','Weiß','Schwarz']
-    },{
-      category:'Maltechnik',
-      term:['Lasur','Mit geschlossenen Augen','mit links','beidhändig','Nass-in-Nass','Collagieren','Spachteln','Aquarell','Acryl','Kreide','Wachskreide','Aqarellstifte','Tinte']
-    },{
-      category:'Emotionen',
-      term:['Freude','Wut','Angst','Ekel','Trauer','Überraschung','Liebe','Müde']
-    },{
-      category:'Muster',
-      term:['Streifen','Punkte','Flecken','Zacken','Karo','Rauten','Schuppen','Kringel','Wellen','Leo','Zebra','Tetris','Sterne','Herzen']
-    }];
-
-  topicList= [{
-    topic:"Tier", 
-    term:['Hund','Katze','Maus','Kuh', 'Huhn', 'Hahn', 'Pferd', 'Schwein', 'Gans', 'Hase', 'Esel', 'Ziege', 'Schaf']
-  },{
-    topic:'Meerestiere',
-    term:['Auster','Garnele','Hai','Clownfisch','Delfin','Krabbe','Rochen','Seepferdchen','Tintenfisch','Pinguin','Oktopus','Walross']
-  },{
-    topic:'Wesen',
-    term:['Drachen','Feen','Zwerge','Hexe','Zauberer','Elfe','Einhorn','Engel']
-  },{
-    topic:'Pflanzen',
-    term:['Baum','Blume','Beere','Obst','Gemüse','Pilze','Kaktus','Gras']
-  },{
-    topic:'Kleidung',
-    term:['Jacke','Hose','Pullover','Schuhe','Mütze','Schal','T-Shirt','Hut']
-  },{
-    topic:'Transportmittel',
-    term:['Trecker','Bagger','Krahn','Auto','Buss','Fahrrad','Flugzeug','Zug','Helikopter','Boot','Motorrad','Roller']
-  }];
 
   result: string[];
 
@@ -54,15 +16,16 @@ export class AppComponent {
     return RANDOM;
   }
 
-  public onGenerateClick(){
+  private collectRandomTerms(list:any):void{
+    list.forEach((item, ind) => {
+      var randomIndex = this.returnRendomIndexFromTermList(list[ind].term.length);
+      this.result.push(item.term[randomIndex]);
+    });
+  }
+
+  public onGenerateClick():void{
     this.result = [];
-    this.topicList.forEach((cat, ind) => {
-      var randomIndex = this.returnRendomIndexFromTermList(this.topicList[ind].term.length);
-      this.result.push(cat.term[randomIndex]);
-    });
-    this.categoryList.forEach((cat, ind) => {
-      var randomIndex = this.returnRendomIndexFromTermList(this.categoryList[ind].term.length);
-      this.result.push(cat.term[randomIndex]);
-    });
+    this.collectRandomTerms(data.default.topicList);
+    this.collectRandomTerms(data.default.categoryList);
   }
 }
